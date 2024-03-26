61e9e7d8d11a94641ac4c5970d9764defe512990
zabitaguliyeva <7644@holbertonstudents.com>
2024-03-26 13:27:30 +0000

    MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+-- A database hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- A new user hbnb_dev (in localhost)
+CREATE USER IF NOT EXISTS 'hbnb_dev' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
