4f6db30491b893e2ed7025aedf07afccf479cf8c
zabitaguliyeva <7644@holbertonstudents.com>
2024-03-26 13:43:38 +0000

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,10 +2,9 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- A new user hbnb_dev (in localhost)
-CREATE USER IF NOT EXISTS 'hbnb_dev' IDENTIFIED BY 'hbnb_dev_pwd';
-
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
 -- hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
