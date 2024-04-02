9c7d37edc82d8ca608ee4f7db083342f30410ac4
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-04-02 15:16:34 +0400

    Update place.py

--- models/place.py
+++ models/place.py
@@ -7,6 +7,15 @@ from os import getenv
 from models.amenity import Amenity
 
 
+association_table = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
+
+
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
@@ -21,20 +30,11 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id'),
-                                 primary_key=True, nullable=False),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id'),
-                                 primary_key=True, nullable=False))
+    reviews = relationship("Review", backref="place", cascade="delete")
+    amenities = relationship("Amenity", secondary="place_amenity",
+                             viewonly=False)
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        reviews = relationship("Review", backref="place",
-                               cascade="all, delete")
-        amenities = relationship("Amenity", secondary=place_amenity,
-                                 viewonly=False)
-    else:
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def amenities(self):
             from models import storage
