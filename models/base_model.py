7b337cdcdafb029fc2938d8f2343809a3b768349
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-04-02 15:42:52 +0400

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-from uuid import uuid4
+import uuid
 from datetime import datetime
 from sqlalchemy import Column, String
 from sqlalchemy.sql.sqltypes import DateTime
