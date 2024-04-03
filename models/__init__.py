c4de8c38fed861473ab67a0ae71ff657f23bdc7f
zabitaguliyeva <7644@holbertonstudents.com>
2024-04-03 17:09:16 +0400

    fix

--- models/__init__.py
+++ models/__init__.py
@@ -3,12 +3,10 @@
 from os import getenv
 
 
-storage = None
-if getenv('HBNB_TYPE_STORAGE') == 'db':
+if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
