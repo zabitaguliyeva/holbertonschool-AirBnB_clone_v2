c815f77c5ad04f54dff2b24a89f6fc8e0037754d
zabitaguliyeva <7644@holbertonstudents.com>
2024-04-03 17:24:20 +0400

    fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,5 @@
--- A database hbnb_dev_db
+-- Prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- A new user hbnb_dev (in localhost)
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
