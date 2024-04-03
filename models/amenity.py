a232752ea4ce80088df74560d950f6dce1c3f48b
zabitaguliyeva <7644@holbertonstudents.com>
2024-04-03 17:07:38 +0400

    fix

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
-    """ Amenity class for HBNB project"""
-    __tablename__ = 'amenities'
+    """Represents an Amenity for a MySQL database.
 
-    name = Column(String(128),
-                  nullable=False)
-    place_amenities = relationship("Place",
-                                   secondary='place_amenity',
-                                   viewonly=False,
-                                   overlaps="amenities")
+    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary='place_amenity',
+                                   overlaps="amenities", viewonly=False)
