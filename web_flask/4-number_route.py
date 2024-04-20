774bcd9620a34c60d7963e57aec69da8cb51e0f0
zabitaguliyeva <7644@holbertonstudents.com>
2024-04-20 12:28:15 +0000

    4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Flask """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Hello Hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """Hello Hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hello_text(text):
+    """Text"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def hello_pytext(text="is cool"):
+
+    """Text"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def hello_num(n):
+
+    """Text"""
+    return "{} is a number".format(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
