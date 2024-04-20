ef30479bc2d2a04aa1b5531dcd082f9a14d09a22
zabitaguliyeva <7644@holbertonstudents.com>
2024-04-20 12:07:55 +0000

    pycodestyle problem was fixed

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,10 +26,11 @@ def hello_text(text):
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def hello_pytext(text = "is cool"):
+def hello_pytext(text="is cool"):
 
     """Text"""
     return "Python {}".format(text.replace("_", " "))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
