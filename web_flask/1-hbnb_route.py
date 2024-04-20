b430868647416c009566e4f852ba06e4cd234998
zabitaguliyeva <7644@holbertonstudents.com>
2024-04-20 10:49:15 +0000

    1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,12 @@ def hello_world():
     """Hello Hbnb"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello_hbnb():
     """Hello Hbnb"""
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
